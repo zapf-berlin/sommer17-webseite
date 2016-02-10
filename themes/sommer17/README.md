c7f85b10bdb0f5d5cbe144f99ddfaa73ac25de73
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2016-02-10 23:47:47 +0100

    Add Sommer17 Theme

---
+++ themes/sommer17/README.md
@@ -0,0 +1,26 @@
+Starter
+=======
+
+A clean base theme ideal for creating new hugo themes. Created by http://www.gregboggs.com
+
+Base features:
+  - Top menu
+  - Main sidebar menu
+  - Correct header markup
+  - Clean body markup
+  - Blog style index page
+  - Pagination
+  - Category list page
+  - Single page template
+  - Post template
+  - Read time and word count on each post
+  - Google Analytics
+  - Code highlighting
+  - Built in SEO, canonical, description, keywords
+
+To Use
+------
+    git clone https://github.com/Greg-Boggs/hugo-starter-kickstart
+    cd hugo-starter-kickstart
+    git clone https://github.com/Greg-Boggs/starter themes/starter
+
---
+++ themes/sommer17/static/js/scroll.js
@@ -0,0 +1,39 @@
+function compute_offset() {
+	return 0.1*$('#header-image').outerHeight();
+}
+function compute_duration() {
+	/* Set the duration according to the height of the image */
+	return $('#header-image').outerHeight()-compute_offset() -12;
+}
+
+var scene = new ScrollMagic.Scene({offset: compute_offset(), duration: compute_duration(), triggerHook: 0})
+	.addIndicators({name:"foo"})
+	.on("enter", function (e) {
+
+		if (e.target.controller().info("scrollDirection") == "FORWARD") {
+			$('html, body').animate({scrollTop: ($('#main-menu').offset().top)}, 400, 'swing', function() {
+				$('#main-menu').addClass('pinned');
+			});
+			scene['duration'](compute_duration());
+		} else {
+			$('#main-menu').removeClass('pinned');
+			$('html, body').animate({scrollTop: 0}, 'slow');
+		}
+	})
+	.addTo(scroller);
+
+
+/* Ensure the body is large enough */
+function minimum_resize() {
+		var minimum_height = $(window).height() - $('#main-footer').outerHeight();
+
+		//var minimum_height = 10000;
+		if ($('#main-content').outerHeight() < minimum_height) {
+			$('#main-content').height(minimum_height);
+		}
+}
+
+$(window).on('resize', minimum_resize);
+$(minimum_resize);
+$(window).on('resize', function() {scene['duration'](compute_duration());});
+$(window).on('resize', function() {scene['offset'](compute_offset());});
